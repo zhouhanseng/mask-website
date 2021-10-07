434b4e7b6fa1ade28cd638f119729c63219727a3
zhouhanseng <z308114274@gmail.com>
2021-10-07 17:09:15 +0800

    chore: add cname

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://mask.io`,
   },
   plugins: [
     "gatsby-plugin-postcss",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    "gatsby-plugin-cname",
   ],
   pathPrefix: "/mask-website",
 };

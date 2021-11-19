3fd0f7488460675c151436b2643cfd96d862e125
billy <Billy@mask.io>
2021-11-19 17:50:35 +0800

    fix: kill console log

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,5 +2,4 @@ export default function buryPointTrigger(evtName: string) {
   const _window = window as any;
   if (!_window.dataLayer) return;
   _window.dataLayer.push({ event: evtName });
-  console.log(window.dataLayer, "sss");
 }

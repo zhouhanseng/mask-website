a663e6186020c746597e47a0d9c5f4d52f9b646e
lelenei <zwpsky@gmail.com>
2023-08-11 09:27:29 +0800

    fix: bugfix

--- src/components/GA.tsx
+++ src/components/GA.tsx
@@ -1,18 +1,21 @@
-import React from”react”
-import {Script} from "gatsby"
+import { Script } from "gatsby";
+import React from "react";
 
 export function GA() {
-    return (
-        <>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HH3XSGH6WT" />
-            <Script id="ga">
-                {`
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HH3XSGH6WT"
+      />
+      <Script id="ga">
+        {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-HH3XSGH6WT');
                 `}
-            </Script>
-        </>
-    )
+      </Script>
+    </>
+  );
 }
